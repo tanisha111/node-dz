ac3f30a918677ed83eecdd627de7454ca4471e46
Танюха <tanuha@MacBook-Pro-1234.local>
2022-08-08 19:03:53 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+//const file = fs.readFileSync('index.html')
+
+//console.log(file.toString())
+
+//fs.promises.readFile('index.html')
+
+///fs.readFile('index.html', (err, data)=> {
+   // if (err) {
+     //   console.log('error', err)
+   // }
+   // console.log(data)
+//})
+
+
