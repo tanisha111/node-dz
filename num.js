3c6bb0ead33e868e87c8e2f4374bf9fbdaac0fac
Танюха <[vesloguzz@icloud.com]>
2022-08-08 22:21:19 +0500

    1dz

--- num.js
+++ num.js
@@ -1,11 +1,8 @@
 import colors from 'colors'
 function isPrime(num) {
-    
-    
-    for (let i=2; i < num; i++) {
+      for (let i=2; i < num; i++) {
         if (num%i === 0) {
-            return 'false';
-            
+            return 'false';        
         }
        
     }   
